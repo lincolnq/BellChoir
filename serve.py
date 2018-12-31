5ff27b0693e146b5c9519c4ce926d9af5701821c
Lincoln Quirk <lincolnquirk@Lincolns-MacBook-Pro.local>
2018-12-30 20:44:46 -0500

    show ip address

--- serve.py
+++ serve.py
@@ -3,6 +3,7 @@ import urlparse
 import urllib
 import re
 import glob
+import socket
 
 import format_sheet
 
@@ -96,10 +97,23 @@ class GetHandler(BaseHTTPRequestHandler):
 
         return open('lyrics.html').read().format(song_title=xml, legend=legend, lyrics=result)
 
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        # doesn't even have to be reachable
+        s.connect(('10.255.255.255', 1))
+        IP = s.getsockname()[0]
+    except:
+        IP = '127.0.0.1'
+    finally:
+        s.close()
+    return IP
+
+
 if __name__ == '__main__':
+    ip = get_ip()
     from BaseHTTPServer import HTTPServer
-    print("Loading song xml")
-    server = HTTPServer(('192.168.3.25', 8000), GetHandler)
-    print 'Starting server, use <Ctrl-C> to stop'
+    server = HTTPServer((ip, 8000), GetHandler)
+    print 'Starting server at http://{}:8000/, use <Ctrl-C> to stop'.format(ip)
     server.serve_forever()
 
